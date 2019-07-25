93e9b1b109f500b29a1de8a20493c3fc92bdbd06
mirarifhsan <arif.ishan05@gmail.com>
2019-07-25 17:43:50 +0600

    Scan done

---
+++ app/src/main/java/com/example/asus/qrcodescanner/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.asus.qrcodescanner;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT =  1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Switching activity to Home Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
